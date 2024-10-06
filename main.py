0a5d683ea76357e66fc7c052bd16806f133cd5df
Nazil Noorani <nooraninazil@gmail.com>
2024-10-06 12:59:05 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+#We will be converting strings to morse code
+
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+user_input = input("Input Text Here:")
+converted_list = []
+output = ""
+for i in user_input:
+    try:
+        converted_list.append(MORSE_CODE_DICT[i.upper()])
+    except:
+        converted_list = []
+        print("Invalid Character in string")
+        break
+
+if len(converted_list) > 0:
+    for i in converted_list:
+        output += i
+    print(output)
+
+
+
